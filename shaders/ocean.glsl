c0b2a76ee25153974fa5dcb460235875a69b94e3
shahd-moh-abdel <shahd.mohammed.abdel@gmail.com>
2025-09-10 13:39:37 +0300

    parse shaders

---
+++ shaders/ocean.glsl
@@ -0,0 +1,7 @@
+#shader vertex
+
+vertex code here
+
+#shader fragment
+
+fragment code here
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,46 @@
+#include <fstream>
 #include <iostream>
 #include "../include/glad/glad.h"
 #include <GLFW/glfw3.h>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
+
+struct shaderProgram {
+  string vertexShader;
+  string fragmentShader;
+};
+
+static shaderProgram parseShader(const string& filePath) {
+  ifstream stream(filePath);
+
+  enum class ShaderType {
+    NONE = -1, VERTEX = 0, FRAGMENT = 1
+  };
+
+  string line;
+  stringstream ss[2];
+  ShaderType type = ShaderType::NONE;
+
+  while (getline(stream, line))
+    {
+      if (line.find("#shader") != string::npos)
+	{
+	  if (line.find("vertex") != string::npos)
+	    type = ShaderType::VERTEX;
+	  else if (line.find("fragment") != string::npos)
+	    type = ShaderType::FRAGMENT;
+	}
+      else
+	ss[(int)type] << line << '\n';
+      
+    }
+  return {ss[0].str(), ss[1].str()}; 
+}
 
 void processInput(GLFWwindow* window)
 {
@@ -38,6 +73,10 @@ GLFWwindow* initWindow()
 int main()
 {
   GLFWwindow* window = initWindow();
+
+  shaderProgram source = parseShader("./shaders/ocean.glsl");
+
+  cout << "Vertex: " << source.vertexShader << '\n' << "Fragment: " << source.fragmentShader << endl;
 
   while (!glfwWindowShouldClose(window))
     {
