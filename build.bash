1f612008d253a749381803f66fe960d904021873
shahd-moh-abdel <shahd.mohammed.abdel@gmail.com>
2025-09-10 17:31:27 +0300

    testing with imgui lib

--- build.bash
+++ build.bash
@@ -1,1 +1,20 @@
-g++ src/main.cpp glad.c -o app -Iinclude -lglfw  -ldl -lX11 -lpthread -lXrandr -lXi
+#!/bin/bash
+
+g++ src/main.cpp \
+    src/imgui/imgui.cpp \
+    src/imgui/imgui_demo.cpp \
+    src/imgui/imgui_draw.cpp \
+    src/imgui/imgui_tables.cpp \
+    src/imgui/imgui_widgets.cpp \
+    src/imgui/imgui_impl_glfw.cpp \
+    src/imgui/imgui_impl_opengl3.cpp \
+    glad.c \
+    -o app \
+    -Iinclude \
+    -Iinclude/imgui \
+    -lglfw \
+    -ldl \
+    -lX11 \
+    -lpthread \
+    -lXrandr \
+    -lXi
--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,18 @@
 #include <sstream>
 #include <string>
 
+
+#include "../include/imgui/imgui.h"
+#include "../include/imgui/imgui_impl_glfw.h"
+#include "../include/imgui/imgui_impl_opengl3.h"
+
 using namespace std;
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
+
+float waveSpeed = 1.0f;
+float waveHeight = 0.5f;
 
 struct shaderProgram {
   string vertexShader;
@@ -50,6 +58,10 @@ void processInput(GLFWwindow* window)
 
 GLFWwindow* initWindow()
 {
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  
   glfwInit();
   GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Ocean", NULL, NULL);
 
@@ -70,10 +82,46 @@ GLFWwindow* initWindow()
   return window;
 }
 
+// creating imgui context 
+void initImGui(GLFWwindow* window)
+{
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+  ImGui::StyleColorsLight();
+
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplOpenGL3_Init("#version 330");
+
+}
+
+void renderImGui()
+{
+  ImGui_ImplOpenGL3_NewFrame();
+  ImGui_ImplGlfw_NewFrame();
+  ImGui::NewFrame();
+
+  ImGui::Begin("Controls");
+  ImGui::Text("this is a text");
+  ImGui::End();
+
+  ImGui::Render();
+  ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
+
+void cleanup()
+{
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
+}
+
 int main()
 {
   GLFWwindow* window = initWindow();
-
+  initImGui(window);
+  
   shaderProgram source = parseShader("./shaders/ocean.glsl");
 
   cout << "Vertex: " << source.vertexShader << '\n' << "Fragment: " << source.fragmentShader << endl;
@@ -81,11 +129,12 @@ int main()
   while (!glfwWindowShouldClose(window))
     {
       processInput(window);
-
+      renderImGui();
       glfwSwapBuffers(window);
       glfwPollEvents();
     }
-  
+
+  cleanup();
   glfwTerminate();
   return 0;
 }
