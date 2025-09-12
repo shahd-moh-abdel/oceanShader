eae1760d6865efc1ace1ceeca6a1675ed94fcd27
shahd-moh-abdel <shahd.mohammed.abdel@gmail.com>
2025-09-12 19:05:09 +0300

    wave function

--- shaders/ocean.glsl
+++ shaders/ocean.glsl
@@ -28,15 +28,21 @@ uniform float u_time;
 //const vec3 skyColor2 = vec3(0.529, 0.808, 0.922);
 //const vec3 sunColor = vec3(1.0, 0.9, 0.7);
 
+float wave(vec2 p, float time)
+{
+  float h = 0.0;
+  h += u_waveHeight * 0.5 * sin(p.x * 1.0 + time * u_waveSpeed);
+  h += u_waveHeight * 0.3 * sin(p.x * 2.0 + p.y * 0.5 + time * u_waveSpeed * 1.3);
+  return h;
+}
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord)
 {
-  vec2 uv = fragCoord.xy / u_resolution.xy;
+  vec2 uv = (fragCoord.xy - 0.5 * u_resolution.xy) / u_resolution.y;
 
-  float wave = sin(uv.x * 10.0 + u_time * u_waveSpeed) * u_waveHeight;
-  wave += sin(uv.y * 8.0 + u_time * u_waveSpeed * 0.7) * u_waveHeight * 0.5;
-  
-  vec3 waveColor = u_color + vec3(wave * 0.2);
+  float waveVal = wave(uv * 5.0, u_time);
+    
+  vec3 waveColor = u_color + waveVal * 0.2;
   
   fragColor = vec4(waveColor,1.0);
 }
